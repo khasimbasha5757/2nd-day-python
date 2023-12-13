7491cef28d458fe8708b220a81672e50d35fe226
SHAIK KHASIM  BASHA <115825218+khasimbasha5757@users.noreply.github.com>
2023-12-13 14:45:49 +0530

    Add files via upload

---
+++ 2nd day.py
@@ -0,0 +1,22 @@
+print("Hello world")
+print(5)
+print("bye")
+print(8+9,17*13)
+print("Hey i am a good boy and the one who is watching the code is also a \ngood boy !")
+print("khasim basha")
+# comments
+#hey boy dont read this line coz its comment
+# Name : shaik khasim basha
+# class :14th 
+'''multiline commands are very goof
+and it is also very bad
+kncwjnwjvbiwvbwbv'''
+# # # single line command are used by - #
+'''  multiple line command is used fby-   
+this is 
+ multi line 
+     ok naa'''
+print ("And now we are going to get the escape character by using \n and it gets us new line;; and now im giving a long space by giving \t it gives us a tab.")
+print(" i am going to right this \" khasimbasha\"")
+print("hey",6,7,8,sep="~",end='005\n')
+print("Harry")
